faa94deef9ab72a7be6035f3fa2b4b857a312aaf
Lucas Holten <lhc.holten@gmail.com>
2022-11-26 01:47:41 +0100

    Rewrite indexing

--- src/index.rs
+++ src/index.rs
@@ -1,4 +1,11 @@
+use std::{
+    iter::{self, once, Once},
+    marker::PhantomData,
+};
+
 use bit_iter::BitIter;
+
+use crate::proj::{Mask, Proj};
 
 #[derive(Default)]
 pub struct Index {
@@ -6,64 +13,195 @@ pub struct Index {
     pub total: usize,
 }
 
-pub trait Choose<T> {
-    fn apply(self, index: T, total: T) -> Self;
-    fn choose_one(self, val: u8, mask: T) -> Self;
-    fn choose_exact(self, vals: T, mask: T) -> Self;
-    fn choose_at_most<const NUM: u32>(self, vals: T, mask: T) -> Self;
+pub trait Indexer: IntoIterator + Sized {
+    fn index(self, board: &Self::Item) -> Index;
+
+    fn choose_one<V, M>(self, proj: V, mask: M) -> Flatten<Self, V, M, ChooseOne>
+    where
+        V: Proj<Self::Item, Output = u8>,
+        M: Mask<Self::Item, Output = u32>,
+    {
+        Flatten {
+            inner: self,
+            proj,
+            mask,
+            gen: ChooseOne,
+        }
+    }
+
+    fn choose_exact<V, M>(self, proj: V, mask: M, count: u8) -> Flatten<Self, V, M, ChooseExact>
+    where
+        V: Proj<Self::Item>,
+        M: Mask<Self::Item>,
+    {
+        Flatten {
+            inner: self,
+            proj,
+            mask,
+            gen: ChooseExact { count },
+        }
+    }
 }
 
-macro_rules! index_impl {
+#[derive(Default)]
+pub struct Empty<T>(PhantomData<T>);
+
+impl<T: Default> IntoIterator for Empty<T> {
+    type Item = T;
+    type IntoIter = Once<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        once(Default::default())
+    }
+}
+
+impl<T: Default> Indexer for Empty<T> {
+    fn index(self, _: &Self::Item) -> Index {
+        Index::default()
+    }
+}
+
+pub struct Flatten<I, V, M, G> {
+    inner: I,
+    proj: V,
+    mask: M,
+    gen: G,
+}
+
+impl<I: IntoIterator, V, M, G> IntoIterator for Flatten<I, V, M, G>
+where
+    V: Proj<I::Item>,
+    M: Mask<I::Item>,
+    G: Gen<M::Output, V::Output>,
+    I::Item: Clone,
+{
+    type Item = I::Item;
+    type IntoIter = impl Iterator<Item = Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.inner.into_iter().flat_map(move |board: I::Item| {
+            let mask: M::Output = self.mask.get_mask(&board);
+            self.gen.gen_iter(mask).map(move |field: V::Output| {
+                let mut new = board.clone();
+                *(self.proj).proj_mut(&mut new) = field;
+                new
+            })
+        })
+    }
+}
+
+impl<I: Indexer, V, M, G> Indexer for Flatten<I, V, M, G>
+where
+    V: Proj<I::Item>,
+    M: Mask<I::Item>,
+    G: Gen<M::Output, V::Output>,
+    I::Item: Clone,
+{
+    fn index(self, board: &I::Item) -> Index {
+        let this = self.inner.index(board);
+        let mask: M::Output = self.mask.get_mask(board);
+        let field: &V::Output = self.proj.proj_ref(board);
+        let other = self.gen.index(mask, field);
+        Index {
+            index: this.index * other.total as usize + other.index as usize,
+            total: this.total * other.total as usize,
+        }
+    }
+}
+
+trait Gen<M, F> {
+    type GenIter: Iterator<Item = F>;
+    fn gen_iter(&self, mask: M) -> Self::GenIter;
+    fn index(&self, mask: M, field: &F) -> Index;
+}
+
+pub struct ChooseOne;
+
+impl Gen<u32, u8> for ChooseOne {
+    type GenIter = impl Iterator<Item = u8>;
+
+    fn gen_iter(&self, mask: u32) -> Self::GenIter {
+        BitIter::from(mask).map(|offset| offset as u8)
+    }
+
+    fn index(&self, mask: u32, offset: &u8) -> Index {
+        let mask_less = (1 << *offset) - 1;
+        Index {
+            index: (mask_less & mask).count_ones() as usize,
+            total: mask.count_ones() as usize,
+        }
+    }
+}
+
+pub struct ChooseExact {
+    count: u8,
+}
+
+impl Gen<u16, u16> for ChooseExact {
+    type GenIter = impl Iterator<Item = u16>;
+
+    fn gen_iter(&self, mask: u16) -> Self::GenIter {
+        debug_assert!(self.count < 5);
+
+        let mut lookup = [0u16; 5];
+        let mut low_mask = 0;
+        for i in 0..=self.count as usize {
+            lookup[self.count as usize - i] = low_mask;
+            let high_bits = mask & !low_mask;
+            low_mask |= high_bits & high_bits.wrapping_neg();
+        }
+
+        println!("{:?}", lookup);
+
+        let mut curr = 0u16;
+        iter::from_fn(move || {
+            let lowest = curr & curr.wrapping_neg();
+
+            let (new, done) = (curr | !mask).overflowing_add(lowest);
+
+            curr = new & mask;
+            curr |= lookup[curr.count_ones() as usize];
+
+            (!done).then_some(curr)
+        })
+    }
+
+    fn index(&self, mask: u16, vals: &u16) -> Index {
+        Index {
+            index: index_exact(*vals as u32, mask as u32),
+            total: comb_exact(mask.count_ones(), vals.count_ones()),
+        }
+    }
+}
+
+macro_rules! gen_impl {
     ($($t:ty)*) => {$(
-        impl Choose<$t> for Index {
-            fn apply(self, n: $t, d: $t) -> Self {
-                Self {
-                    index: self.index * d as usize + n as usize,
-                    total: self.total * d as usize
+        impl Gen<$t, $t> for ChooseExact {
+            type GenIter = impl Iterator<Item = $t>;
+
+            fn gen_iter(&self, mask: $t) -> Self::GenIter {
+                // TODO: fix this
+                BitIter::from(mask).map(|offset| offset as $t)
+                // BitIter::from(mask).flat_map(|offset| {
+                //     let mask_less = (1 << offset) - 1;
+                //     let new_mask = mask & mask_less;
+                //     todo!()
+                // })
+            }
+
+            fn index(&self, mask: $t, vals: &$t) -> Index {
+                Index {
+                    index: index_exact(*vals as u32, mask as u32),
+                    total: comb_exact(mask.count_ones(), vals.count_ones()),
                 }
-            }
-            fn choose_one(self, val: u8, mask: $t) -> Self {
-                let mask_less = (1 << val) - 1;
-                let num_less = (mask_less & mask).count_ones();
-                let num_total = mask.count_ones();
-                self.apply(num_less, num_total)
-            }
-            fn choose_exact(self, vals: $t, mask: $t) -> Self {
-                let index = index_exact(vals as u32, mask as u32);
-                let total = comb_exact(mask.count_ones(), vals.count_ones());
-                self.apply(index, total)
-            }
-            fn choose_at_most<const NUM: u32>(self, vals: $t, mask: $t) -> Self {
-                let index = index_at_most::<NUM>(vals as u32, mask as u32);
-                let total = comb_at_most(mask.count_ones(), NUM);
-                self.apply(index, total)
             }
         }
     )*}
 }
 
-index_impl! { u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize }
+gen_impl! { u8 u32 u64 u128 usize i8 i16 i32 i64 i128 isize }
 
-pub fn index_at_most<const MAX: u32>(vals: u32, mask: u32) -> u64 {
-    debug_assert!(vals.count_ones() <= MAX as u32);
-    debug_assert_eq!(vals & !mask, 0);
-
-    let mut i = 0;
-    let mut max = MAX;
-    for offset in BitIter::from(vals).rev() {
-        let mask_less = (1 << offset) - 1;
-        let num_less = (mask_less & mask).count_ones();
-        i += comb_at_most(num_less, max);
-        max -= 1;
-    }
-    i
-}
-
-pub fn comb_at_most(num_less: u32, max: u32) -> u64 {
-    (0..=max).map(|count| comb_exact(num_less, count)).sum()
-}
-
-pub fn index_exact(vals: u32, mask: u32) -> u64 {
+pub fn index_exact(vals: u32, mask: u32) -> usize {
     debug_assert_eq!(vals & !mask, 0);
 
     let mut i = 0;
@@ -75,30 +213,25 @@ pub fn index_exact(vals: u32, mask: u32) -> u64 {
     i
 }
 
-pub fn comb_exact(num_less: u32, count: u32) -> u64 {
+pub fn comb_exact(num_less: u32, count: u32) -> usize {
     if count as u32 > num_less {
         return 0;
     }
 
     let n: u64 = (0..count as u64).map(|i| num_less as u64 - i).product();
     let d: u64 = (1..=count as u64).product();
-    n / d
+    (n / d) as usize
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::comb_exact;
+    use super::{comb_exact, ChooseExact, Gen};
 
     #[test]
     fn comb_some() {
-        assert_eq!(comb_exact(0, 0), 1);
-        assert_eq!(comb_exact(1, 0), 1);
-        assert_eq!(comb_exact(1, 1), 2);
-        assert_eq!(comb_exact(2, 1), 3);
-        assert_eq!(comb_exact(2, 2), 4);
-        assert_eq!(comb_exact(3, 2), 7);
-
-        assert_eq!(comb_exact(1, 2), 2);
+        ChooseExact { count: 2 }
+            .gen_iter(0b1111u16)
+            .for_each(|x| println!("{x:04b}"))
     }
 }
--- src/onitama.rs
+++ src/onitama.rs
@@ -1,49 +1,64 @@
 use crate::{
     card::cards_mask,
-    index::{Choose, Index},
+    index::{Empty, Index, Indexer},
+    proj,
 };
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Default, Clone, Copy)]
 pub struct Board {
     pawns: [u32; 2],
     cards: [u16; 2],
     kings: [u8; 2],
 }
 
-impl Board {
-    const TEMPLES: [u8; 2] = [2, 22];
-    const TABLE_MASK: u32 = (1 << 25) - 1;
+#[derive(Clone, Default)]
+pub struct IncompleteBoard {
+    pawns: [u8; 2], // number of pawns for each side
+    cards: [u16; 2],
+    king1: u8,
+}
 
+const TEMPLES: [u8; 2] = [2, 22];
+const TABLE_MASK: u32 = (1 << 25) - 1;
+const NUM_PAWNS_MASK: u32 = 0b11111; // five options, because there can be 0..=4 pawns
+
+impl IncompleteBoard {
     // Index boards that are not win in 1
     // we could save a little more by restricting opp king loc
     // but that would need an additional indirection
-    pub fn index1(&self, all_cards: u16) -> Index {
-        let cards0_mask = all_cards;
-        let cards1_mask = all_cards & !self.cards[0];
-        let kings1_mask = Self::TABLE_MASK & !(1 << Self::TEMPLES[0]);
-        let pawns0_count = self.pawns[0].count_ones() as u8;
+    pub fn index1(all_cards: u16) -> impl Indexer<Item = Self> {
+        type B = IncompleteBoard;
 
-        Index::default()
-            .choose_exact(self.cards[0], cards0_mask)
-            .choose_exact(self.cards[1], cards1_mask)
-            .choose_one(self.kings[1], kings1_mask)
-            .apply(pawns0_count, 5)
+        let cards1_mask = move |b: &B| all_cards & !b.cards[0];
+        let king1_mask = TABLE_MASK & !(1 << TEMPLES[0]);
+
+        Empty::default()
+            .choose_exact(proj!(|b: B| b.cards[0]), all_cards, 2)
+            .choose_exact(proj!(|b: B| b.cards[1]), cards1_mask, 2)
+            .choose_one(proj!(|b: B| b.king1), king1_mask)
+            .choose_one(proj!(|b: B| b.pawns[0]), NUM_PAWNS_MASK)
+            .choose_one(proj!(|b: B| b.pawns[1]), NUM_PAWNS_MASK)
     }
+}
 
+impl Board {
     // Second half of the index, the size of this table depends on the first index
-    pub fn index2(&self) -> Index {
-        let kings0_mask = Self::TABLE_MASK
-            & !(1 << Self::TEMPLES[1])
-            & !cards_mask::<true>(Self::TEMPLES[1], self.cards[0])
-            & !cards_mask::<true>(self.kings[1], self.cards[0]);
-        let kings_mask = Self::TABLE_MASK & !(1 << self.kings[0]) & !(1 << self.kings[1]);
-        let pawns0_skip = kings_mask & !cards_mask::<true>(self.kings[1], self.cards[0]);
-        let pawns1_skip = kings_mask & !self.pawns[0];
+    pub fn index2(first: IncompleteBoard) -> impl Indexer<Item = Self> {
+        type B = Board;
 
-        Index::default()
-            .choose_one(self.kings[0], kings0_mask)
-            .choose_exact(self.pawns[0], pawns0_skip)
-            .choose_at_most::<4>(self.pawns[1], pawns1_skip)
+        let kings0_mask = TABLE_MASK
+            & !(1 << TEMPLES[1])
+            & !cards_mask::<true>(TEMPLES[1], first.cards[0])
+            & !cards_mask::<true>(first.king1, first.cards[0]);
+        let kings_mask = move |b: &B| TABLE_MASK & !(1 << first.king1) & !(1 << b.kings[0]);
+        let pawns0_skip =
+            move |b: &B| kings_mask(b) & !cards_mask::<true>(first.king1, first.cards[0]);
+        let pawns1_skip = move |b: &B| kings_mask(b) & !b.pawns[0];
+
+        Empty::default()
+            .choose_one(proj!(|b: B| b.kings[0]), kings0_mask)
+            .choose_exact(proj!(|b: B| b.pawns[0]), pawns0_skip, first.pawns[0])
+            .choose_exact(proj!(|b: B| b.pawns[1]), pawns1_skip, first.pawns[1])
     }
 
     // // generate pawn moves that do not result in obv win for opp
