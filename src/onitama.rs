c63cc474dfb9553acdd0587f870172c78df292b7
Lucas Holten <lhc.holten@gmail.com>
2022-11-26 13:08:19 +0100

    Cleanup cards and onitama

--- src/onitama.rs
+++ src/onitama.rs
@@ -1,6 +1,6 @@
 use crate::{
     card::cards_mask,
-    index::{Empty, Index, Indexer},
+    index::{Empty, Indexer},
     proj,
 };
 
@@ -12,53 +12,66 @@ pub struct Board {
 }
 
 #[derive(Clone, Default)]
-pub struct IncompleteBoard {
-    pawns: [u8; 2], // number of pawns for each side
+pub struct BoardIncomplete {
+    pawn_n: [u8; 2], // number of pawns for each side
     cards: [u16; 2],
     king1: u8,
 }
 
-const TEMPLES: [u8; 2] = [2, 22];
+// team 0 is at the bottom, so that they can use the cards unrotated
+const TEMPLES: [u8; 2] = [22, 2];
 const TABLE_MASK: u32 = (1 << 25) - 1;
 const NUM_PAWNS_MASK: u32 = 0b11111; // five options, because there can be 0..=4 pawns
 
-impl IncompleteBoard {
+impl BoardIncomplete {
     // Index boards that are not win in 1
     // we could save a little more by restricting opp king loc
     // but that would need an additional indirection
     pub fn index1(all_cards: u16) -> impl Indexer<Item = Self> {
-        type B = IncompleteBoard;
+        type B = BoardIncomplete;
 
         let cards1_mask = move |b: &B| all_cards & !b.cards[0];
         let king1_mask = TABLE_MASK & !(1 << TEMPLES[0]);
 
         Empty::default()
-            .choose_exact(proj!(|b: B| b.cards[0]), all_cards, 2)
-            .choose_exact(proj!(|b: B| b.cards[1]), cards1_mask, 2)
+            .choose(2, proj!(|b: B| b.cards[0]), all_cards)
+            .choose(2, proj!(|b: B| b.cards[1]), cards1_mask)
             .choose_one(proj!(|b: B| b.king1), king1_mask)
-            .choose_one(proj!(|b: B| b.pawns[0]), NUM_PAWNS_MASK)
-            .choose_one(proj!(|b: B| b.pawns[1]), NUM_PAWNS_MASK)
+            .choose_one(proj!(|b: B| b.pawn_n[0]), NUM_PAWNS_MASK)
+            .choose_one(proj!(|b: B| b.pawn_n[1]), NUM_PAWNS_MASK)
     }
 }
 
 impl Board {
+    pub fn incomplete(&self) -> BoardIncomplete {
+        BoardIncomplete {
+            pawn_n: [
+                self.pawns[0].count_ones() as u8,
+                self.pawns[1].count_ones() as u8,
+            ],
+            cards: self.cards,
+            king1: self.kings[1],
+        }
+    }
+
     // Second half of the index, the size of this table depends on the first index
-    pub fn index2(first: IncompleteBoard) -> impl Indexer<Item = Self> {
+    pub fn index2(first: BoardIncomplete) -> impl Indexer<Item = Self> {
         type B = Board;
 
-        let kings0_mask = TABLE_MASK
-            & !(1 << TEMPLES[1])
-            & !cards_mask::<true>(TEMPLES[1], first.cards[0])
-            & !cards_mask::<true>(first.king1, first.cards[0]);
-        let kings_mask = move |b: &B| TABLE_MASK & !(1 << first.king1) & !(1 << b.kings[0]);
-        let pawns0_skip =
-            move |b: &B| kings_mask(b) & !cards_mask::<true>(first.king1, first.cards[0]);
-        let pawns1_skip = move |b: &B| kings_mask(b) & !b.pawns[0];
+        let temple1 = 1 << TEMPLES[1];
+        let king0 = |b: &B| 1 << b.kings[0];
+        let king1 = 1 << first.king1;
+        let attack_temple1 = cards_mask::<true>(TEMPLES[1], first.cards[0]);
+        let attack_kings1 = cards_mask::<true>(first.king1, first.cards[0]);
+
+        let kings0_mask = TABLE_MASK & !temple1 & !king1 & !attack_temple1 & !attack_kings1;
+        let pawns0_mask = move |b: &B| TABLE_MASK & !king1 & !king0(b) & !attack_kings1;
+        let pawns1_mask = move |b: &B| TABLE_MASK & !king1 & !king0(b) & !b.pawns[0];
 
         Empty::default()
             .choose_one(proj!(|b: B| b.kings[0]), kings0_mask)
-            .choose_exact(proj!(|b: B| b.pawns[0]), pawns0_skip, first.pawns[0])
-            .choose_exact(proj!(|b: B| b.pawns[1]), pawns1_skip, first.pawns[1])
+            .choose(first.pawn_n[0], proj!(|b: B| b.pawns[0]), pawns0_mask)
+            .choose(first.pawn_n[1], proj!(|b: B| b.pawns[1]), pawns1_mask)
     }
 
     // // generate pawn moves that do not result in obv win for opp
